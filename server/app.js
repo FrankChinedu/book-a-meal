a44d884abb5e6362bad046c824d6e82f1e9845fe
obi chinedu frank <frankieetchy@gmail.com>
2019-02-08 20:19:00 +0100

    make ui responsive

--- server/app.js
+++ server/app.js
@@ -6,10 +6,15 @@ const morgan = require('morgan');
 const app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-app.get('/s', (req, res) => {
-  res.send('something');
+app.get('*', (req, res) => {
+  res.send({
+    message: 'someher',
+  });
 });
 
-app.listen(4000, () => console.log('running on port 4000'));
+// app.listen(4000, () => console.log('running on port 4000'));
+
+module.exports = app;
---
+++ server/server/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+app.get('/s', (req, res) => {
+  res.send('something');
+});
+
+app.listen(4000, () => console.log('running on port 4000'));
